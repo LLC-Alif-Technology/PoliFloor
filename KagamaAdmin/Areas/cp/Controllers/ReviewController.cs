6ec8fec0cd469867c871bc76e9c324e3b80d5ac0
dvmuhammad <davlatovmuhammad1264@gmail.com>
2022-01-27 14:53:34 +0500

    add table review

---
+++ KagamaAdmin/Areas/cp/Controllers/ReviewController.cs
@@ -0,0 +1,38 @@
+﻿using KagamaAdmin.Areas.cp.ViewModels.Region;
+using KagamaModels;
+using KagamaModels.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KagamaAdmin.Areas.cp.Controllers
+{
+    [Area("cp")]
+    [Authorize]
+    public class ReviewController : Controller
+    {
+        private IKagamaRepository _repository;
+        private IHostingEnvironment _appEnvironment;
+
+        public ReviewController(IKagamaRepository repository, IHostingEnvironment appEnvironment)
+        {
+            _repository = repository;
+            _appEnvironment = appEnvironment;
+        }
+
+        public async Task<List<Review>> Test()
+        {
+            var run = await _repository.GetAllReview();
+            return run;
+        }
+
+    }
+}   
